b17201e7f1dd7063157b36629e4f6225826271e8
PCWALLACE <pratyushchhibber@gmail.com>
2019-01-17 19:21:16 +0530

    first commit

---
+++ src/chatapp/networking/Writable_GUI.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapp.networking;
+
+/**
+ *
+ * @author chhib
+ */
+public interface Writable_GUI {
+    
+    void write(String s);
+}
